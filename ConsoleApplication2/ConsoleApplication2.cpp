eca7c537fb371afa3d277222acd28abae749e413
scandiy <38243615+scandiy@users.noreply.github.com>
2024-02-05 09:43:14 +0200

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+
+
+int main() {
+    // Введення даних
+    std::string name;
+    int age;
+
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    // Розрахунок року народження
+    time_t now = time(0);
+    tm* ltm = localtime(&now);
+    int currentYear = 1900 + ltm->tm_year;
+    int birthYear = currentYear - age;
+
+    // Виведення результату
+    std::cout << "Hi, " << name << "! Your birth year: " << birthYear << std::endl;
+
+    return 0;
+}
